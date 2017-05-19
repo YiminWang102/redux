0a862946646e2ef8ed2773695fb289461b7fdeaa
Yi Min Wang <yimin@Yis-MacBook-Pro.local>
2017-05-19 15:37:34 -0400

    something up with songs

--- browser/react/components/Lyrics.js
+++ browser/react/components/Lyrics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ({text, setArtist, artistQuery, setSong, songQuery, handleSubmit}) {
+export default function ({lyrics, setArtist, artistQuery, setSong, songQuery, handleSubmit}) {
 
 	const artistChange = function (event) {
 		const artist = event.target.value;
@@ -20,7 +20,7 @@ export default function ({text, setArtist, artistQuery, setSong, songQuery, hand
 			<input type="text" onChange={artistChange} placeholder="Artist" name="artist" value={artistQuery} />
 			<input type="text" onChange={songChange} placeholder="Song" name="song" value={songQuery} />
 			</div>
-			<pre>{ text || 'Search above' }</pre>
+			<pre>{ lyrics.text || 'Search above' }</pre>
 			<button type="submit">Search for Lyrics</button>
 		</form>
 		</div>
--- browser/react/store.js
+++ browser/react/store.js
@@ -1,8 +1,16 @@
-import {createStore, applyMiddleware} from 'redux';
-import reducer from './reducers/root-reducer';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+//import reducer from './reducers/root-reducer';
 import loggerMiddleware from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
+import playerReducer from './reducers/player-reducer';
+import lyricsReducer from './reducers/lyrics-reducer';
+
 const middleWare = applyMiddleware(loggerMiddleware, thunkMiddleware);
+
+const reducer = combineReducers({
+  lyrics: lyricsReducer,
+  player: playerReducer
+})
 
 export default createStore(reducer, middleWare);
